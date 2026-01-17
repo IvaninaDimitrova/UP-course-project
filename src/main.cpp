8912bcb9d8378af18d44f2a393c8b8ce4cd7e74a
Ivanina Dimitrova <ivaninadimitrova134@gmail.com>
2026-01-17 10:38:47 +0200

    Added main menu and char function

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,54 @@
 *
 */
 
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+int strLen(const char s[]) {
+    int i = 0;
+    while (s[i] != '\0') i++;
+    return i;
+}
+
+bool strEqual(const char a[], const char b[]) {
+    int i = 0;
+    while (a[i] != '\0' && b[i] != '\0') {
+        if (a[i] != b[i]) return false;
+        i++;
+    }
+    return a[i] == b[i];
+}
+
+void strCopy(char dest[], const char src[]) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
 int main() {
-	return 0;
+	while (true) {
+    cout << "\n--- WORDLE ---\n";
+    cout << "1. Login\n";
+    cout << "2. Sign in\n";
+    cout << "3. Exit\nChoice: ";
+    int choice;
+    cin >> choice;
+    cin.ignore();
+
+    if (choice == 1) {
+        login();
+    }
+    else if (choice == 2) {
+        registerUser();
+    }
+    else {
+        break;
+    }
+}
+
+return 0;
 }
